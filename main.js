6366eea80dae520585492ff99a41df25ecda9b7d
marios <mgeorgak859@gmail.com>
2023-08-20 17:17:30 +0300

    adds game board

--- main.js
+++ main.js
@@ -1,24 +1,68 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import * as THREE from 'three';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-setupCounter(document.querySelector('#counter'))
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+camera.position.set(1, 1, 100);
+
+// Create the TicTacToe board
+const board = new THREE.Group();
+
+// Vertical lines
+const verticalLineGeometry = new THREE.BoxGeometry(4, 64, 4);
+const verticalLineMaterial = new THREE.MeshBasicMaterial({color: 0x327fa8});
+
+const verticalLineLeft = new THREE.Mesh(verticalLineGeometry, verticalLineMaterial);
+verticalLineLeft.position.set(-30, 0, 12);
+board.add(verticalLineLeft);
+
+
+const verticalLineMiddleLeft = new THREE.Mesh(verticalLineGeometry, verticalLineMaterial);
+verticalLineMiddleLeft.position.set(-10, 0, 12);
+board.add(verticalLineMiddleLeft);
+
+const verticalLineRight = new THREE.Mesh(verticalLineGeometry, verticalLineMaterial);
+verticalLineRight.position.set(30, 0, 12);
+board.add(verticalLineRight);
+
+const verticalLineMiddleRight = new THREE.Mesh(verticalLineGeometry, verticalLineMaterial);
+verticalLineMiddleRight.position.set(10, 0, 12);
+board.add(verticalLineMiddleRight);
+
+
+// Horizontal lines
+const horizontalLineGeometry = new THREE.BoxGeometry(64, 4, 4);
+const horizontalLineMaterial = new THREE.MeshBasicMaterial({color: 0x327fa8});
+
+const horizontalLineTop = new THREE.Mesh(horizontalLineGeometry, horizontalLineMaterial);
+horizontalLineTop.position.set(0, 30, 12);
+board.add(horizontalLineTop);
+
+const horizontalLineMiddleTop = new THREE.Mesh(horizontalLineGeometry, horizontalLineMaterial);
+horizontalLineMiddleTop.position.set(0, 10, 12);
+board.add(horizontalLineMiddleTop);
+
+const horizontalLineBottom = new THREE.Mesh(horizontalLineGeometry, horizontalLineMaterial);
+horizontalLineBottom.position.set(0, -30, 12);
+board.add(horizontalLineBottom);
+
+
+const horizontalLineMiddleBottom = new THREE.Mesh(horizontalLineGeometry, horizontalLineMaterial);
+horizontalLineMiddleBottom.position.set(0, -10, 12);
+board.add(horizontalLineMiddleBottom);
+
+
+scene.add(board);
+
+function animate() {
+	requestAnimationFrame(animate);
+    board.rotation.y += 0.002;
+	renderer.render(scene, camera);
+}
+
+animate();
+
