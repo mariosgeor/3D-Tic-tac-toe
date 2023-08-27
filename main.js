dbefc50d3e0537d1a594687064f73b42535c38de
marios <marios.geor@hotmail.com>
2023-08-27 12:41:32 +0300

    adds loop to create cubes

--- main.js
+++ main.js
@@ -92,51 +92,24 @@ board.add(horizontalLineMiddleBottom);
 const cubeGeometry = new THREE.BoxGeometry(10, 10, 10);
 const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 
-const cube9 = new THREE.Mesh(cubeGeometry, cubeMaterial);
-const initialPosition9 = new THREE.Vector3(20, -20, 12);
-cube9.position.copy(initialPosition9);
-hiddenCubesGroup.add(cube9);
-cube9.visible =false;
-const cube8 = new THREE.Mesh(cubeGeometry, cubeMaterial);
-const initialPosition8 = new THREE.Vector3(0, -20, 12);
-cube8.position.copy(initialPosition8);
-hiddenCubesGroup.add(cube8);
-cube8.visible =false;
-const cube7 = new THREE.Mesh(cubeGeometry, cubeMaterial);
-const initialPosition7 = new THREE.Vector3(-20, -20, 12);
-cube7.position.copy(initialPosition7);
-hiddenCubesGroup.add(cube7);
-cube7.visible =false;
-const cube6 = new THREE.Mesh(cubeGeometry, cubeMaterial);
-const initialPosition6 = new THREE.Vector3(20, 0, 12);
-cube6.position.copy(initialPosition6);
-hiddenCubesGroup.add(cube6);
-cube6.visible =false;
-const cube5 = new THREE.Mesh(cubeGeometry, cubeMaterial);
-const initialPosition5 = new THREE.Vector3(0, 0, 12);
-cube5.position.copy(initialPosition5);
-hiddenCubesGroup.add(cube5);
-cube5.visible =false;
-const cube4 = new THREE.Mesh(cubeGeometry, cubeMaterial);
-const initialPosition4 = new THREE.Vector3(-20, 0, 12);
-cube4.position.copy(initialPosition4);
-hiddenCubesGroup.add(cube4);
-cube4.visible =false;
-const cube3 = new THREE.Mesh(cubeGeometry, cubeMaterial);
-const initialPosition3 = new THREE.Vector3(20, 20, 12);
-cube3.position.copy(initialPosition3);
-hiddenCubesGroup.add(cube3);
-cube3.visible =false;
-const cube1 = new THREE.Mesh(cubeGeometry, cubeMaterial);
-const initialPosition1 = new THREE.Vector3(-20, 20, 12);
-cube1.position.copy(initialPosition1);
-hiddenCubesGroup.add(cube1);
-cube1.visible =false;
-const cube2 = new THREE.Mesh(cubeGeometry, cubeMaterial);
-const initialPosition2 = new THREE.Vector3(-0, 20, 12);
-cube2.position.copy(initialPosition2);
-hiddenCubesGroup.add(cube2);
-cube2.visible =false;
+const cubePositions = [
+  new THREE.Vector3(20, -20, 12),
+  new THREE.Vector3(0, -20, 12),
+  new THREE.Vector3(-20, -20, 12),
+  new THREE.Vector3(20, 0, 12),
+  new THREE.Vector3(0, 0, 12),
+  new THREE.Vector3(-20, 0, 12),
+  new THREE.Vector3(20, 20, 12),
+  new THREE.Vector3(-20, 20, 12),
+  new THREE.Vector3(0, 20, 12)
+];
+
+cubePositions.forEach(position => {
+  const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+  cube.position.copy(position);
+  cube.visible = false;
+  hiddenCubesGroup.add(cube);
+});
 
 const players = ["X", "O"];
 const playerMeshes = {
