135470136c0976af22c57b1c1db09f917b2c07b9
marios <marios.geor@hotmail.com>
2023-08-30 15:47:50 +0300

    adds function on mouse down to check for win

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import * as THREE from "three";
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -9,24 +9,23 @@ const camera = new THREE.PerspectiveCamera(
 );
 
 const renderer = new THREE.WebGLRenderer({
-  antialias:true
+  antialias: true,
 });
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setPixelRatio(window.devicePixelRatio)
+renderer.setPixelRatio(window.devicePixelRatio);
 document.body.appendChild(renderer.domElement);
 
 camera.position.z = 100;
 
 const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true; 
+controls.enableDamping = true;
 controls.dampingFactor = 0.05;
 
 const board = new THREE.Group();
 const hiddenCubesGroup = new THREE.Group();
 const gameSymbols = new THREE.Group();
 
-
-let currentPlayer = "sphere"; 
+let currentPlayer = "sphere";
 
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
 scene.add(ambientLight);
@@ -34,7 +33,6 @@ scene.add(ambientLight);
 const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
 directionalLight.position.set(1, 1, 1);
 scene.add(directionalLight);
-
 
 const material = new THREE.MeshStandardMaterial({
   color: 0xfcc742,
@@ -57,13 +55,14 @@ const linePositions = [
   { position: new THREE.Vector3(0, -31.5, 12), horizontal: true },
 ];
 
-linePositions.forEach(line => {
-  const lineMesh = new THREE.Mesh(line.horizontal ? horizontalLineGeometry : lineGeometry, material);
+linePositions.forEach((line) => {
+  const lineMesh = new THREE.Mesh(
+    line.horizontal ? horizontalLineGeometry : lineGeometry,
+    material
+  );
   lineMesh.position.copy(line.position);
   board.add(lineMesh);
 });
-
-
 
 const cubeGeometry = new THREE.BoxGeometry(10, 10, 10);
 const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
@@ -77,10 +76,10 @@ const cubePositions = [
   new THREE.Vector3(-20, 0, 12),
   new THREE.Vector3(20, 20, 12),
   new THREE.Vector3(-20, 20, 12),
-  new THREE.Vector3(0, 20, 12)
+  new THREE.Vector3(0, 20, 12),
 ];
 
-cubePositions.forEach(position => {
+cubePositions.forEach((position) => {
   const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
   cube.position.copy(position);
   cube.visible = false;
@@ -90,23 +89,17 @@ cubePositions.forEach(position => {
 const players = ["X", "O"];
 const playerMeshes = {
   X: createXMesh(),
-  O: createEarthMesh(), 
+  O: createEarthMesh(),
 };
 let currentPlayerIndex = 0;
 
-
-
-
 const raycaster = new THREE.Raycaster();
 const mouse = new THREE.Vector2();
-
-
 
 renderer.domElement.addEventListener("mousedown", onMouseDown);
 scene.add(board);
 scene.add(hiddenCubesGroup);
 scene.add(gameSymbols);
-
 
 function createXMesh() {
   const xGroup = new THREE.Group();
@@ -124,20 +117,15 @@ function createXMesh() {
   xArm2.rotation.z = 3.6;
   xGroup.add(xArm2);
 
-
-
   return xGroup;
 }
 
-
 function createEarthMesh() {
   const earthGeometry = new THREE.SphereGeometry(5, 50, 50);
-  const earthMaterial = new THREE.MeshBasicMaterial({ 
+  const earthMaterial = new THREE.MeshBasicMaterial({
     // color: 0xff0000
-    map: new THREE.TextureLoader().load(
-      './assets/images/globe.jpg'
-    )
-   });
+    map: new THREE.TextureLoader().load("./assets/images/globe.jpg"),
+  });
   const earth = new THREE.Mesh(earthGeometry, earthMaterial);
   return earth;
 }
@@ -151,7 +139,7 @@ function animate() {
     playerMesh.rotation.y += 0.008;
   });
 
-  controls.update(); 
+  controls.update();
 
   renderer.render(scene, camera);
 }
@@ -168,17 +156,75 @@ function onMouseDown(event) {
     const clickedCube = intersects[0].object;
 
     if (!clickedCube.userData.clicked) {
-      clickedCube.userData.clicked = true; 
+      clickedCube.userData.clicked = true;
       clickedCube.visible = false;
 
       const currentPlayer = players[currentPlayerIndex];
       const playerMesh = playerMeshes[currentPlayer].clone();
       playerMesh.position.copy(clickedCube.position);
+      clickedCube.userData.player = currentPlayer;
 
+      playerMesh.userData.symbol = (currentPlayer === players[0]) ? 'X' : 'sphere';
       gameSymbols.add(playerMesh);
-      clickedCubes.push(clickedCube); 
+      
+      clickedCubes.push(clickedCube);
+
+      console.log(`Current Player: ${currentPlayer}`);
 
       currentPlayerIndex = (currentPlayerIndex + 1) % players.length;
+
+      function checkWin(player) {
+        let pos8 = { x: 20, y: -20, z: 12 };
+        let pos7 = { x: 0, y: -20, z: 12 };
+        let pos6 = { x: -20, y: -20, z: 12 };
+        let pos5 = { x: 20, y: 0, z: 12 };
+        let pos4 = { x: 0, y: 0, z: 12 };
+        let pos3 = { x: -20, y: 0, z: 12 };
+        let pos2 = { x: 20, y: 20, z: 12 };
+        let pos0 = { x: -20, y: 20, z: 12 };
+        let pos1 = { x: 0, y: 20, z: 12 };
+        const winConditions = [
+          [pos0, pos1, pos2],
+          [pos3, pos4, pos5],
+          [pos6, pos7, pos8],
+          [pos0, pos3, pos6],
+          [pos1, pos4, pos7],
+          [pos2, pos5, pos8],
+          [pos0, pos4, pos8],
+          [pos2, pos4, pos6],
+        ];
+        function formatVector(vector) {
+          return `{x: ${vector.x}, y: ${vector.y}, z: ${vector.z}}`;
+        }
+        if (gameSymbols) {
+          const positionsToCheck = [pos0, pos1, pos2, pos3, pos4, pos5, pos6, pos7, pos8];
+        
+          for (const condition of winConditions) {
+            const matchingSymbols = [];
+            
+            for (const position of condition) {
+              const matchingChild = gameSymbols.children.find((gameSymbol) => {
+                return gameSymbol.position.equals(position);
+              });
+        
+              if (matchingChild) {
+                matchingSymbols.push(matchingChild);
+              }
+            }
+        
+            if (matchingSymbols.length === 3) {
+              const playerSymbol = (player === players[0]) ? 'X' : 'sphere';
+              
+              const allMatched = matchingSymbols.every(symbol => symbol.userData.symbol === playerSymbol);
+              if (allMatched) {
+                console.log(`Player ${player} wins!`);
+                return true; 
+              }
+            }
+          }
+        }
+      }
+      checkWin(currentPlayer);
 
       // Custom scale-up animation for the player mesh
       let scaleValue = 0;
@@ -210,7 +256,7 @@ function onWindowResize() {
   renderer.setSize(newWidth, newHeight);
 }
 
-const starGeometry = new THREE.BufferGeometry()
+const starGeometry = new THREE.BufferGeometry();
 const starMaterial = new THREE.PointsMaterial({
   color: 0xffffff,
 });
@@ -220,20 +266,17 @@ const starVertices = [];
 for (let i = 0; i < 10000; i++) {
   const x = (Math.random() - 0.5) * 2000;
   const y = (Math.random() - 0.5) * 2000;
-  const z = (Math.random() - 0.5) * 2000; 
+  const z = (Math.random() - 0.5) * 2000;
   starVertices.push(x, y, z);
 }
 
 starGeometry.setAttribute(
-  'position',
+  "position",
   new THREE.Float32BufferAttribute(starVertices, 3)
 );
 
 const stars = new THREE.Points(starGeometry, starMaterial);
 scene.add(stars);
-
-
-
 
 onWindowResize();
 
